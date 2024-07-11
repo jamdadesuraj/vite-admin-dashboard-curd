48a35a2544eb0bf518ce8970cd0649f91a85ab75
unknown <surajjamdade83086@gmail.com>
2024-07-11 17:25:06 +0530

    first commit

---
+++ src/routes/sections/dashboard.tsx
@@ -0,0 +1,76 @@
+import { lazy, Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import { AuthGuard } from 'src/auth/guard';
+import DashboardLayout from 'src/layouts/dashboard';
+
+import { LoadingScreen } from 'src/components/loading-screen';
+
+// ----------------------------------------------------------------------
+
+// OVERVIEW
+const IndexPage = lazy(() => import('src/pages/dashboard/app'));
+
+const OverviewFilePage = lazy(() => import('src/pages/dashboard/file'));
+
+// USER
+const UserProfilePage = lazy(() => import('src/pages/dashboard/user/profile'));
+const UserCardsPage = lazy(() => import('src/pages/dashboard/user/cards'));
+const UserListPage = lazy(() => import('src/pages/dashboard/user/list'));
+const UserAccountPage = lazy(() => import('src/pages/dashboard/user/account'));
+const UserCreatePage = lazy(() => import('src/pages/dashboard/user/new'));
+const UserEditPage = lazy(() => import('src/pages/dashboard/user/edit'));
+
+// FILE MANAGER
+const FileManagerPage = lazy(() => import('src/pages/dashboard/file-manager'));
+// APP
+const ChatPage = lazy(() => import('src/pages/dashboard/chat'));
+const MailPage = lazy(() => import('src/pages/dashboard/mail'));
+const CalendarPage = lazy(() => import('src/pages/dashboard/calendar'));
+const KanbanPage = lazy(() => import('src/pages/dashboard/kanban'));
+// TEST RENDER PAGE BY ROLE
+const PermissionDeniedPage = lazy(() => import('src/pages/dashboard/permission'));
+// BLANK PAGE
+const BlankPage = lazy(() => import('src/pages/dashboard/blank'));
+
+// ----------------------------------------------------------------------
+
+export const dashboardRoutes = [
+  {
+    path: 'dashboard',
+    element: (
+      <AuthGuard>
+        <DashboardLayout>
+          <Suspense fallback={<LoadingScreen />}>
+            <Outlet />
+          </Suspense>
+        </DashboardLayout>
+      </AuthGuard>
+    ),
+    children: [
+      { element: <IndexPage />, index: true },
+
+      { path: 'file', element: <OverviewFilePage /> },
+      {
+        path: 'user',
+        children: [
+          { element: <UserProfilePage />, index: true },
+          { path: 'profile', element: <UserProfilePage /> },
+          { path: 'cards', element: <UserCardsPage /> },
+          { path: 'list', element: <UserListPage /> },
+          { path: 'new', element: <UserCreatePage /> },
+          { path: ':id/edit', element: <UserEditPage /> },
+          { path: 'account', element: <UserAccountPage /> },
+        ],
+      },
+
+      { path: 'file-manager', element: <FileManagerPage /> },
+      { path: 'mail', element: <MailPage /> },
+      { path: 'chat', element: <ChatPage /> },
+      { path: 'calendar', element: <CalendarPage /> },
+      { path: 'kanban', element: <KanbanPage /> },
+      { path: 'permission', element: <PermissionDeniedPage /> },
+      { path: 'blank', element: <BlankPage /> },
+    ],
+  },
+];
